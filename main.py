19d609a3a73c5efd73c97c23696da9cc91e6cefa
_D1zast3R_ <petru00000@mail.ru>
2024-12-02 20:15:55 +0300

    log dumping + main assembler func

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import xml.etree.ElementTree
+from xml.etree.ElementTree import ElementTree
+
+
 class Solution:
     def __init__(self, input_file: str, log_file: str, output_file: str):
         self.namespace: dict = {}
         self.input_file: str = input_file
         self.log_file: str = log_file
-        self.output_file = output_file
+        self.output_file: str = output_file
         self.free_address: int = -1
-        self.logs: list[str] = []
+        self.logs: list = []
 
     def paginate(self, num: int, length: int) -> str:
         return "0" * (length - len(bin(num)[2:])) + bin(num)[2:]
@@ -14,7 +18,7 @@ class Solution:
         if method == "last":
             self.logs.append(text)
         elif method == "append":
-            self.logs.update(text)
+            self.logs[-1].update(text)
 
     def get_free_adress(self) -> int:
         self.free_address += 1
@@ -60,6 +64,71 @@ class Solution:
         fixed_zeroes = all_combined + '0' * (48 - len(all_combined))
         return int(fixed_zeroes, 2).to_bytes(6)
 
+    def log_dump(self):
+        with open(self.log_file, 'r') as f:
+            exporter = ElementTree()
+            exporter.write(self.log_file, 'utf-8')
+
+    def main(self):
+        with open(self.input_file, 'r') as f:
+            lines = f.readlines()
+        for line in lines:
+            if line.startswith("const"):
+                _, var, value = line.split()
+
+                if var not in self.namespace.keys():
+                    address_to = self.add_var_to_namespace(var)
+                else:
+                    address_to = self.namespace[var]
+
+                address = self.add_var_to_namespace(var)
+                binary = self.constant_handler(36, address, int(value))
+                self.output_binary(binary)
+
+            elif line.startswith("move"):
+                _, TO, FROM, shift = line.split()
+
+                if FROM not in self.namespace.keys():
+                    raise Exception(f"Variable {FROM} not declaired.")
+                if TO not in self.namespace.keys():
+                    address_to = self.add_var_to_namespace(TO)
+                else:
+                    address_to = self.namespace[TO]
+
+                address_from = self.namespace[FROM]
+                binary = self.read_handler(58, address_to, address_from, shift)
+                self.output_binary(binary)
+
+            elif line.startswith("write"):
+                _, FROM, TO = line.split()
+
+                if FROM not in self.namespace.keys():
+                    raise Exception(f"Variable {FROM} not declaired.")
+                if TO not in self.namespace.keys():
+                    address_to = self.add_var_to_namespace(TO)
+                else:
+                    address_to = self.namespace[TO]
+
+                address_from = self.namespace[FROM]
+                binary = self.write_handler(25, address_from, address_to)
+                self.output_binary(binary)
+
+            elif line.startswith("mult"):
+                _, var, mult2, mult1 = line.split()
+
+                if mult1 not in self.namespace.keys():
+                    raise Exception(f"Variable {mult1} not declaired.")
+                if mult2 not in self.namespace.keys():
+                    raise Exception(f"Variable {mult2} not declaired.")
+                if var not in self.namespace.keys():
+                    address = self.add_var_to_namespace(var)
+                else:
+                    address = self.namespace[var]
+
+                address_mult1 = self.namespace[mult1]
+                address_mult2 = self.namespace[mult2]
+                binary = self.mult_handler(32, address, address_mult2, address_mult1)
+                self.output_binary(binary)
 
 
 if __name__ == "__main__":
